c234d082f427ccdf4205287ec7c1745ec1ea7c69
Ankit parashar <152596388+CARBO889@users.noreply.github.com>
2024-08-18 23:54:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const hour = document.getElementById('hour');
+const minute = document.getElementById('minute');
+const second = document.getElementById('second');
+const day = document.getElementById('day');
+
+var monthName = [
+    'January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'
+];
+
+const clock = setInterval(function time() {
+    let today = new Date();
+    let d = today.getDate();
+    let m = today.getMonth();
+    let y = today.getFullYear();
+    let h = today.getHours();
+    let min = today.getMinutes();
+    let sec = today.getSeconds();
+
+    // Add leading zeros if necessary
+    h = (h < 10) ? "0" + h : h;
+    min = (min < 10) ? "0" + min : min;
+    sec = (sec < 10) ? "0" + sec : sec;
+
+    day.innerHTML = `${d} ${monthName[m]} ${y}`;
+    hour.textContent = h;
+    minute.textContent = min;
+    second.textContent = sec;
+}, 1000); // Update the time every second
+
+
